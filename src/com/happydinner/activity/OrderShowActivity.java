98c95613d7cede519731848386c8b5c658f2a9b0
lihb <liqixi_169@126.com>
2015-05-20 18:52:18 +0800

    增加显示订单activity

---
+++ src/com/happydinner/activity/OrderShowActivity.java
@@ -0,0 +1,20 @@
+package com.happydinner.activity;
+
+import android.os.Bundle;
+import com.happydinner.base.BaseActivity;
+
+/**
+ * 类说明：
+ *
+ * @author Administrator
+ * @version 1.0
+ * @date 2015/5/20
+ */
+
+public class OrderShowActivity extends BaseActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
