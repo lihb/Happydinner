3bd100845fe7d87f4ba8fe4f0b0eaf28f2007235
lihb <liqixi_169@126.com>
2015-06-16 17:25:36 +0800

    增加付款activity和布局文件

---
+++ src/com/happydinner/activity/PayActivity.java
@@ -0,0 +1,21 @@
+package com.happydinner.activity;
+
+import android.os.Bundle;
+import com.happydinner.base.BaseActivity;
+
+/**
+ * 类说明：
+ *
+ * @author Administrator
+ * @version 1.0
+ * @date 2015/6/16
+ */
+
+public class PayActivity extends BaseActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
